5d538dea27f668b2e2992d87e35f6c0e3805c433
longvm <longvm@abigames.com.vn>
2024-07-23 16:20:12 +0700

    add wait for utils

---
+++ Assets/_Game/Scripts/Common/Utils/WaitForUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForUtils
+{
+    class FloatComparer : IEqualityComparer<float>
+    {
+        public bool Equals(float x, float y) => Mathf.Abs(x - y) <= Mathf.Epsilon;
+        public int GetHashCode(float obj) => obj.GetHashCode();
+    }
+
+    static readonly WaitForEndOfFrame endOfFrame = new WaitForEndOfFrame();
+    public static WaitForEndOfFrame EndOfFrame => endOfFrame;
+
+    static readonly Dictionary<float, WaitForSeconds> waitForSecondsDictionary = new(100, new FloatComparer());
+
+    public static WaitForSeconds Seconds(float seconds)
+    {
+        if (seconds < 1f / Application.targetFrameRate) return null;
+
+        if (!waitForSecondsDictionary.TryGetValue(seconds, out var wait))
+        {
+            wait = new WaitForSeconds(seconds);
+            waitForSecondsDictionary.Add(seconds, wait);
+        }
+
+        return wait;
+    }
+}
